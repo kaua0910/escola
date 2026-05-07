f1e80b28a618dae10c894a260258514e1b33cac9
kaka <kaua.alameida0910@gmail.com>
2026-05-06 21:14:19 -0300

    adicionar calculator

---
+++ calculator/src/Calculator.java
@@ -0,0 +1,40 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+
+        if (args[0].equals("soma")) {
+            sum(x, y);
+
+        }else if (args[0].equals("subtrair")) {
+            sub(x, y);
+        }
+else if (args[0].equals("multiplicar"))
+      mult(x, y);
+else if (args[0].equals("dividir"))
+    div(x, y);
+        else {
+            System.out.println(("nenhuma opcao"));
+        }
+        {
+
+    }
+    }
+
+    static void sum(int x, int y) {
+
+        System.out.println((x + y));
+
+    }
+    static void sub(int x, int y) {
+
+        System.out.println(x - y);
+    }
+    static void mult(int x, int y) {
+        System.out.println(x * y);
+    }
+    static void div(int x, int y) {
+        System.out.println(x / y);
+    }
+}
